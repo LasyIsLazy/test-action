4cad5efc491001c5e9d1b54fcbe0b301ad246b53
Lasy <1255352506@qq.com>
2020-03-09 18:41:14 +0800

    ffz

--- t2.js
+++ t2.js
@@ -1,2 +1,17 @@
+const fs = require('fs')
 const path = require('path')
-console.log(path.posix.join( 's/', 'a'))
+function getAllFilePaths(curPath, paths = []) {
+  const dir = fs.readdirSync(curPath)
+  dir.forEach(item => {
+    const itemPath = path.join(curPath, item)
+    const stat = fs.lstatSync(itemPath)
+    if (stat.isDirectory()) {
+      getAllFilePaths(itemPath, paths)
+    } else {
+      const realPath = fs.realpathSync(itemPath)
+      paths.push(realPath)
+    }
+  })
+  return paths
+}
+console.log(getAllFilePaths('t1'))
