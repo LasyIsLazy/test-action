a280c3d4f0d121d346923e3542bbcba2b2f05036
Lasy <1255352506@qq.com>
2020-03-09 17:50:25 +0800

    as

--- upload-action/index.js
+++ upload-action/index.js
@@ -2,13 +2,10 @@ const upload = require('./upload.js')
 const core = require('@actions/core')
 const now = new Date()
 console.log('上传', now.toLocaleString())
-const repo = `img`
-const username = `LasyIsLazy`
-const remoteDir = '/test3/tt'
 upload(Buffer.from(now.toLocaleString()).toString('base64'), {
-  Authorization: `Bearer ${core.getInput('ACCESS_TOKEN')}`,
-  fileName: now.getTime(),
-  repo,
-  username,
-  remoteDir
+  Authorization: `Bearer ${core.getInput('access-token')}`,
+  username: core.getInput('username'),
+  repo: core.getInput('repo'),
+  remoteDir: core.getInput('remote-dir'),
+  fileName: core.getInput('file-name') || now.getTime()
 })
